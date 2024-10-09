89d8e522b873e57c64b94fd218d9976106a87505
AbheeshKumar <abheshkhemani@gmail.com>
2024-10-09 17:22:16 +0500

    db testing

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+"""
+Django COmmand to wait for db to be available
+"""
+
+import time
+from psycopg2 import OperationalError as Psycog2Error
+from django.db.utils import OperationalError
+
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        """ "EntryPoint for command"""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (Psycog2Error, OperationalError):
+                self.stdout.write("Database Unavailable, waiting 1 sec...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database Available"))
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,39 @@
+from unittest.mock import patch
+
+# Connecting to db before its ready
+from psycopg2 import OperationalError as Psycog2Error
+
+# Calling command by name
+from django.core.management import call_command
+
+# Database exception
+from django.db.utils import OperationalError
+
+# Base test class for testing
+from django.test import SimpleTestCase
+
+
+@patch("core.management.commands.wait_for_db.Command.check")
+class CommandTests(SimpleTestCase):
+    def test_wait_for_db_ready(self, patched_check):
+        """Test Waiting for db if db already ready"""
+        patched_check.return_value = True
+
+        call_command("wait_for_db")
+        patched_check.assert_called_once_with(databases=["default"])
+
+    # Usually there is a sleep interval between each time database is called for tests
+    # This is useful in production but in testing, it slows test down so we remove it
+    @patch("time.sleep")
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test Waiting for database when getting OperationalError"""
+        # Checks if db is operational, 2 times psycog2Error is called to check if postgres is initialized
+        # and 3 times OperationalError is called to check if devdb is created
+        # in 6th time, True returns db successfully launched
+        patched_check.side_effect = [Psycog2Error] * 2 + [OperationalError] * 3 + [True]
+        print("Delay")
+        call_command("wait_for_db")
+
+        # 6 times patched_check is called, 2+3+1
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(databases=["default"])
