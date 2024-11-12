bc796e7c94dcb07f4226efd8a7bc283c8bf5fa04
AbheeshKumar <abheshkhemani@gmail.com>
2024-11-12 15:45:34 +0500

    deploy errors

--- proxy/run.sh
+++ proxy/run.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+echo "Running run.sh"
+
 #environment substitute
 #/etc/nginx/conf.d/default.conf contains default config evs that will substituted in our default proxy
 envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+
 nginx -g 'daemon off;'
 
-
+echo "Completing run.sh"
